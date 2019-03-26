b8bfb46375046aaee50282c7ddc0770aaecf19e6
Bertrand DICKELE <bdickele@gmail.com>
2019-03-26 21:50:49 +0100

    Minor fix

--- app/src/main/java/org/dickele/workout/util/StringUtil.java
+++ app/src/main/java/org/dickele/workout/util/StringUtil.java
@@ -3,7 +3,9 @@ package org.dickele.workout.util;
 import org.dickele.workout.data.WorkoutExercise;
 
 import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public final class StringUtil {
@@ -25,14 +27,19 @@ public final class StringUtil {
     }
 
     private static String getStringForDelta(final int delta) {
-        return "("
-                + (delta < 0 ? "-" : (delta > 0 ? "+" : ""))
-                + delta + ")";
+        return "(" + (delta > 0 ? "+" : "") + delta + ")";
     }
 
     public static String getStringForReps(final List<Integer> reps) {
         if (reps == null || reps.isEmpty()) {
             return "";
+        }
+
+        if (reps.size() > 1) {
+            final Set<Integer> distinctValues = new HashSet<>(reps);
+            if (distinctValues.size() == 1) {
+                return reps.size() + " x " + reps.get(0);
+            }
         }
 
         return reps.stream()
