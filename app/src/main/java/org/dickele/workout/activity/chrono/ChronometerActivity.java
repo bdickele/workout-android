298f474eb68727d10ff2161c1c160e7738ac5fa1
Bertrand DICKELE <bdickele@gmail.com>
2019-08-01 13:49:47 +0200

    Implementation of chronometer

--- app/src/main/java/org/dickele/workout/activity/chrono/ChronometerActivity.java
+++ app/src/main/java/org/dickele/workout/activity/chrono/ChronometerActivity.java
@@ -5,6 +5,8 @@ import android.os.SystemClock;
 import android.view.MenuItem;
 import android.widget.Chronometer;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -25,12 +27,28 @@ public class ChronometerActivity extends AppCompatActivity {
 
     // --------------------------------------------
 
+    private boolean intervalometerRunning;
+
+    private long intervalometerPauseOffset = 0;
+
     private Chronometer interval;
 
-    private long intervalBase = SystemClock.elapsedRealtime();
+    private Chronometer intervalometer;
+
+    private ImageButton buttonIntervalometerPlay;
+
+    private int intervalCounter = 0;
 
     // When adding or substracting seconds to interval, here is the value to add/substract (in ms)
-    private final int intervalOffset = 30000;
+    private static final int INTERVAL_OFFSET = 2 * 1000;
+
+    private int numberOfLapCurrent = 0;
+
+    private int numberOfLapTarget = 0;
+
+    private boolean numberOfLapTargetReached = true;
+
+    private TextView textNbLap;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -46,10 +64,20 @@ public class ChronometerActivity extends AppCompatActivity {
         final ImageButton buttonChronoStop = findViewById(R.id.button_chrono_stop);
         buttonChronoStop.setOnClickListener(v -> stopChrono());
 
+        intervalometerRunning = false;
+
+        textNbLap = findViewById(R.id.nbLap);
         interval = findViewById(R.id.interval);
-        interval.setBase(intervalBase);
-        findViewById(R.id.button_interval_plus).setOnClickListener(v -> addInterval());
-        findViewById(R.id.button_interval_minus).setOnClickListener(v -> substractInterval());
+        intervalometer = findViewById(R.id.intervalometer);
+        interval.setBase(now());
+        findViewById(R.id.button_interval_plus).setOnClickListener(v -> increaseInterval());
+        findViewById(R.id.button_interval_minus).setOnClickListener(v -> decreaseInterval());
+        buttonIntervalometerPlay = findViewById(R.id.button_intervalometer_play);
+        buttonIntervalometerPlay.setOnClickListener(v -> playOrPauseIntervalometer());
+        findViewById(R.id.button_intervalometer_stop).setOnClickListener(v -> stopIntervalometer());
+        numberOfLapCurrent = 0;
+        updateIntervalText();
+        defineIntervalometerListener();
 
         setSupportActionBar(findViewById(R.id.toolbar_main));
         // Used to display back button in toolbar
@@ -68,6 +96,10 @@ public class ChronometerActivity extends AppCompatActivity {
         }
     }
 
+    private long now() {
+        return SystemClock.elapsedRealtime();
+    }
+
     // -------------------------------------------------------------------
 
     private void playOrPauseChrono() {
@@ -81,7 +113,7 @@ public class ChronometerActivity extends AppCompatActivity {
     private void playChrono() {
         chronoRunning = true;
         buttonChronoPlay.setImageResource(R.drawable.ic_pause_black_32dp);
-        chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+        chronometer.setBase(now() - pauseOffset);
         chronometer.start();
     }
 
@@ -89,24 +121,95 @@ public class ChronometerActivity extends AppCompatActivity {
         chronoRunning = false;
         buttonChronoPlay.setImageResource(R.drawable.ic_play_black_32dp);
         chronometer.stop();
-        pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+        pauseOffset = now() - chronometer.getBase();
     }
 
     private void stopChrono() {
         pauseChrono();
-        chronometer.setBase(SystemClock.elapsedRealtime());
+        chronometer.setBase(now());
         pauseOffset = 0;
     }
 
     // -------------------------------------------------------------------
 
-    private void addInterval() {
-        intervalBase += intervalOffset;
-        //interval.setBase(SystemClock.elapsedRealtime() + 1000);
-        interval.setBase(intervalBase);
+    private void increaseInterval() {
+        intervalCounter++;
+        interval.setBase(now() - intervalCounter * INTERVAL_OFFSET);
+        updateIntervalText();
     }
 
-    private void substractInterval() {
+    private void decreaseInterval() {
+        intervalCounter--;
+        if (intervalCounter < 0) {
+            intervalCounter = 0;
+        }
+        updateIntervalText();
+    }
 
+    private void updateIntervalText() {
+        interval.setBase(now() - intervalCounter * INTERVAL_OFFSET);
+    }
+
+    private void playOrPauseIntervalometer() {
+        if (!intervalometerRunning) {
+            playIntervalometer();
+        } else {
+            pauseIntervalometer();
+        }
+    }
+
+    private void playIntervalometer() {
+        if (intervalCounter < 1) {
+            return;
+        }
+        // Clicking on that play button means we've just completed a lap
+        if (numberOfLapTargetReached) {
+            numberOfLapCurrent++;
+            updateNumberOfLap();
+            numberOfLapTarget = numberOfLapCurrent + 1;
+        }
+
+        intervalometerRunning = true;
+        buttonIntervalometerPlay.setImageResource(R.drawable.ic_pause_black_32dp);
+        intervalometer.setBase(now() - intervalometerPauseOffset);
+        intervalometer.start();
+    }
+
+    private void pauseIntervalometer() {
+        intervalometerRunning = false;
+        buttonIntervalometerPlay.setImageResource(R.drawable.ic_play_black_32dp);
+        intervalometer.stop();
+        intervalometerPauseOffset = now() - intervalometer.getBase();
+    }
+
+    private void stopIntervalometer() {
+        pauseIntervalometer();
+        intervalometer.setBase(now());
+        intervalometerPauseOffset = 0;
+        numberOfLapCurrent = 0;
+        numberOfLapTarget = 1;
+        updateNumberOfLap();
+    }
+
+    private void updateNumberOfLap() {
+        textNbLap.setText("" + numberOfLapCurrent);
+    }
+
+    private void defineIntervalometerListener() {
+        intervalometer.setOnChronometerTickListener(chrono -> {
+            if (!intervalometerRunning) {
+                return;
+            }
+
+            final long timeElapsed = now() - chrono.getBase();
+            final long timeTarget = (numberOfLapTarget - 1) * INTERVAL_OFFSET;
+            if (timeElapsed >= timeTarget) {
+                numberOfLapTargetReached = true;
+                pauseIntervalometer();
+                Toast.makeText(this, "Pause terminée : commencer série " + (numberOfLapCurrent + 1), Toast.LENGTH_SHORT).show();
+            } else {
+                numberOfLapTargetReached = false;
+            }
+        });
     }
 }
